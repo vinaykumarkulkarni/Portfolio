c7698b7f6143721187f092e73c70781aec1ac8e4
vinay kulkarni <vinay.kulkarni.kpl@gmail.com>
2026-09-19 13:51:42 +0530

    Next config changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  basePath: "/Portfolio",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
